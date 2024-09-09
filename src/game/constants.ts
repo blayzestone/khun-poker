d82d20a726648303f0c283a5f9edf9495e72de14
Blayze Stone <blayzeostone@gmail.com>
2024-09-09 15:31:16 -0400

    cfr function solves khun poker

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -3,3 +3,8 @@ export const CardRankings = {
   Q: 1,
   J: 0,
 };
+
+export const N_ACTIONS = 2;
+
+// Number of possible card pairs that can be dealt.
+export const CHANCE_PR = 1 / 6;
--- src/game/khunGame.ts
+++ src/game/khunGame.ts
@@ -1,4 +1,4 @@
-import { CardRankings } from "./constants";
+import { CardRankings, CHANCE_PR, N_ACTIONS } from "./constants";
 import { Card } from "./types";
 
 type Action = "p" | "b";
@@ -21,6 +21,20 @@ class KhunGame {
     return infoSet;
   }
 
+  chanceEvent(): number {
+    const deck: Card[] = ["J", "Q", "K"];
+    const numCombinations = 6;
+    let expectedValue = 0;
+    // Run cfr for all combinations of dealt hands
+    for (const c1 of deck) {
+      for (const c2 of deck) {
+        if (c1 === c2) continue;
+        expectedValue += this.cfr("", c1, c2);
+      }
+    }
+    return expectedValue / numCombinations;
+  }
+
   isTerminal(history: string) {
     return [
       "pp", //  Both players passed
@@ -31,86 +45,173 @@ class KhunGame {
     ].includes(history);
   }
 
-  payoff(history: string, player: Card, opponent: Card): number {
-    if (history === "pp") {
+  payoff(history: string, card1: Card, card2: Card): number {
+    const isPlayer1 = history.length % 2 === 0;
+    const player = isPlayer1 ? card1 : card2;
+    const opponent = isPlayer1 ? card2 : card1;
+
+    if (history === "bp" || history === "pbp") {
+      return 1;
+    } else if (history === "pp") {
       return CardRankings[player] > CardRankings[opponent] ? 1 : -1;
     }
-    if (history === "bb" || history === "pbb") {
-      return CardRankings[player] > CardRankings[opponent] ? 2 : -2;
-    }
-    if (history === "bp") {
-      return 1;
-    }
-    if (history === "pbp") {
-      return -1;
-    }
-    return 0;
+    return CardRankings[player] > CardRankings[opponent] ? 2 : -2;
   }
 
   getActions(): Action[] {
     return ["p", "b"];
   }
 
-  playGame(
-    p1: Card,
-    p2: Card,
+  cfr(
     history: string = "",
-    traversingPlayer = 0
+    c1: Card | null = null,
+    c2: Card | null = null,
+    pr1: number = 1,
+    pr2: number = 1
   ): number {
+    if (!c1 || !c2) {
+      return this.chanceEvent();
+    }
     if (this.isTerminal(history)) {
-      const payoff = this.payoff(history, p1, p2);
-      // If P2 is the player traversing than reverse the payoff values
-      if (traversingPlayer === 1) {
-        return -payoff;
+      return this.payoff(history, c1, c2);
+    }
+
+    const isPlayer1 = history.length % 2 === 0;
+    const infoSet = this.getInfoSet(isPlayer1 ? c1 : c2, history);
+    const strategy = infoSet.strategy;
+
+    if (isPlayer1) infoSet.reachPr += pr1;
+    else infoSet.reachPr += pr2;
+
+    const actionUtils: number[] = [0, 0];
+    const actions = this.getActions();
+    for (let i = 0; i < actions.length; i++) {
+      const nextHistory = history + actions[i];
+      if (isPlayer1) {
+        actionUtils[i] =
+          -1 * this.cfr(nextHistory, c1, c2, pr1 * strategy[i], pr2);
+      } else {
+        actionUtils[i] =
+          -1 * this.cfr(nextHistory, c1, c2, pr1, pr2 * strategy[i]);
       }
-      return payoff;
-    }
-    const activePlayer = history.length % 2 ? p1 : p2;
-
-    const infoSet = this.getInfoSet(activePlayer, history);
-    const utils: number[] = [];
-    for (const a of this.getActions()) {
-      const nextTraversingPlayer = activePlayer === p1 ? 0 : 1;
-      utils.push(this.playGame(p1, p2, history + a, nextTraversingPlayer));
     }
 
-    const strategyEV = utils.reduce(
-      (acc, util, i) => (acc += util * infoSet.strategy[i]),
+    const util = actionUtils.reduce(
+      (acc, util, i) => (acc += util * strategy[i]),
       0
     );
 
-    for (let i = 0; i < utils.length; i++) {
-      infoSet.regretSum[i] += utils[i] - strategyEV;
+    // if (c1 === "K") {
+    //   if (isPlayer1) {
+    //     console.log(c1 + history, actionUtils);
+    //   } else {
+    //     console.log(c2 + history, actionUtils);
+    //   }
+    //   console.log("strategy util", util);
+    // }
+
+    const regrets = actionUtils.map((actionUtil) => actionUtil - util);
+    if (isPlayer1) {
+      infoSet.regretSum = regrets.map((r, i) => {
+        return infoSet.regretSum[i] + pr2 * CHANCE_PR * r;
+      });
+    } else {
+      infoSet.regretSum = regrets.map((r, i) => {
+        return infoSet.regretSum[i] + pr1 * CHANCE_PR * r;
+      });
     }
-    return strategyEV;
+
+    return util;
   }
 }
 
-export function dealCards(): [Card, Card] {
-  const deck: Card[] = ["J", "Q", "K"];
-  return [
-    deck.splice(Math.floor(Math.random() * deck.length), 1)[0],
-    deck.splice(Math.floor(Math.random() * deck.length), 1)[0],
-  ];
+export function displayResults(ev: number, infoSets: Map<string, InfoSet>) {
+  console.log("Player 1 expected value:", ev.toFixed(10));
+  console.log("Player 2 expected value:", (-ev).toFixed(10));
+  console.log(" ");
+
+  console.log("Player 1 strategies:");
+  for (const key of infoSets.keys()) {
+    if (key.length === 2) continue;
+    const infoSet = infoSets.get(key);
+    if (infoSet === undefined) continue;
+
+    const strategy = infoSet.avgStrategy().map((v) => v.toFixed(2));
+    console.log(key, strategy);
+  }
+  console.log(" ");
+
+  console.log("Player 2 strategies:");
+  for (const key of infoSets.keys()) {
+    if (key.length !== 2) continue;
+    const infoSet = infoSets.get(key);
+    if (infoSet === undefined) continue;
+
+    const strategy = infoSet.avgStrategy().map((v) => v.toFixed(2));
+    console.log(key, strategy);
+  }
 }
 
 export function Game() {
   const game = new KhunGame();
 
-  const [p1, p2] = dealCards();
-  game.playGame(p1, p2);
-
-  for (const key of game.infoSets.keys()) {
-    console.log(key, game.infoSets.get(key));
+  const iterations = 100 * 1000;
+  let ev = 0;
+  for (let i = 0; i < iterations; i++) {
+    ev += game.cfr();
+    for (const key of game.infoSets.keys()) {
+      const infoSet = game.infoSets.get(key);
+      infoSet?.nextStrategy();
+    }
   }
+  ev /= iterations;
+
+  displayResults(ev, game.infoSets);
 }
 
 class InfoSet {
-  strategy: number[];
   regretSum: number[];
+  strategySum: number[];
+  strategy: number[];
+  reachPr: number;
+  reachPrSum: number;
   constructor() {
-    this.strategy = [0.5, 0.5];
-    this.strategy = strategy;
     this.regretSum = [0, 0];
+    this.strategySum = [0, 0];
+    this.strategy = [1 / N_ACTIONS, 1 / N_ACTIONS];
+    this.reachPr = 0;
+    this.reachPrSum = 0;
+  }
+
+  nextStrategy() {
+    this.strategySum = this.strategy.map(
+      (r, i) => r * this.reachPr + this.strategySum[i]
+    );
+
+    this.strategy = this.calcStrategy();
+    this.reachPrSum += this.reachPr;
+    this.reachPr = 0;
+  }
+
+  calcStrategy() {
+    const regrets = this.regretSum.map((r) => Math.max(r, 0));
+    const total = regrets.reduce((acc, r) => (acc += r));
+
+    if (total > 0) {
+      this.strategy = regrets.map((r) => r / total);
+    } else {
+      // If the total regrets are 0 than fallback to a uniform strategy
+      this.strategy = this.strategy.map(() => 1 / N_ACTIONS);
+    }
+    return this.strategy;
+  }
+
+  avgStrategy() {
+    const strategy = this.strategySum
+      .map((r) => r / this.reachPrSum)
+      .map((r) => (r < 0.001 ? 0 : r)); // remove any values that are likely a mistake
+
+    const total = strategy.reduce((acc, r) => (acc += r));
+    return strategy.map((r) => r / total);
   }
 }
