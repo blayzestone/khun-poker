cfff8157ff3bf7edb63bb53bbf217591f9f8fba4
Blayze Stone <blayzeostone@gmail.com>
2024-09-08 23:29:34 -0400

    wip

--- src/game/khunGame.ts
+++ src/game/khunGame.ts
@@ -1,158 +1,116 @@
-import { GameNode, nodeFactory, TerminalNode } from "./node";
-import { Action, Card } from "./types";
+import { CardRankings } from "./constants";
+import { Card } from "./types";
+
+type Action = "p" | "b";
+
+class KhunGame {
+  infoSets: Map<string, InfoSet>;
+
+  constructor() {
+    this.infoSets = new Map();
+  }
+
+  getInfoSet(card: Card, history: string) {
+    const key = card + history;
+    const infoSet = this.infoSets.get(key);
+    if (infoSet === undefined) {
+      const newInfoset = new InfoSet();
+      this.infoSets.set(key, newInfoset);
+      return newInfoset;
+    }
+    return infoSet;
+  }
+
+  isTerminal(history: string) {
+    return [
+      "pp", //  Both players passed
+      "bb", //  Both players bet
+      "bp", //  Player 2 folded
+      "pbp", // Player 1 folded
+      "pbb", // Player 1 called
+    ].includes(history);
+  }
+
+  payoff(history: string, player: Card, opponent: Card): number {
+    if (history === "pp") {
+      return CardRankings[player] > CardRankings[opponent] ? 1 : -1;
+    }
+    if (history === "bb" || history === "pbb") {
+      return CardRankings[player] > CardRankings[opponent] ? 2 : -2;
+    }
+    if (history === "bp") {
+      return 1;
+    }
+    if (history === "pbp") {
+      return -1;
+    }
+    return 0;
+  }
+
+  getActions(): Action[] {
+    return ["p", "b"];
+  }
+
+  playGame(
+    p1: Card,
+    p2: Card,
+    history: string = "",
+    traversingPlayer = 0
+  ): number {
+    if (this.isTerminal(history)) {
+      const payoff = this.payoff(history, p1, p2);
+      // If P2 is the player traversing than reverse the payoff values
+      if (traversingPlayer === 1) {
+        return -payoff;
+      }
+      return payoff;
+    }
+    const activePlayer = history.length % 2 ? p1 : p2;
+
+    const infoSet = this.getInfoSet(activePlayer, history);
+    const utils: number[] = [];
+    for (const a of this.getActions()) {
+      const nextTraversingPlayer = activePlayer === p1 ? 0 : 1;
+      utils.push(this.playGame(p1, p2, history + a, nextTraversingPlayer));
+    }
+
+    const strategyEV = utils.reduce(
+      (acc, util, i) => (acc += util * infoSet.strategy[i]),
+      0
+    );
+
+    for (let i = 0; i < utils.length; i++) {
+      infoSet.regretSum[i] += utils[i] - strategyEV;
+    }
+    return strategyEV;
+  }
+}
+
+export function dealCards(): [Card, Card] {
+  const deck: Card[] = ["J", "Q", "K"];
+  return [
+    deck.splice(Math.floor(Math.random() * deck.length), 1)[0],
+    deck.splice(Math.floor(Math.random() * deck.length), 1)[0],
+  ];
+}
 
 export function Game() {
   const game = new KhunGame();
 
-  // for (const entry of game.beliefs.keys()) {
-  //   console.log(entry, game.beliefs.get(entry));
-  // }
-}
+  const [p1, p2] = dealCards();
+  game.playGame(p1, p2);
 
-export function getOtherCards(player: Card): Card[] {
-  const cards: Card[] = ["J", "Q", "K"];
-  return cards.filter((c) => c !== player);
-}
-
-class KhunGame {
-  root: GameNode;
-  strategy: InfoSet;
-  beliefs: InfoSet;
-  cumulativeWeights: InfoSet;
-
-  constructor() {
-    const cards: [Card, Card] = ["Q", "J"];
-    this.root = nodeFactory(cards);
-
-    this.strategy = new InfoSet();
-    this.beliefs = new InfoSet();
-    this.cumulativeWeights = new InfoSet();
-
-    this.initStrategy(this.root);
-    this.updateBeliefs(this.root, cards[0], cards[1]);
-
-    this.strategy.update(
-      {
-        hand: "Q",
-        actions: ["pass", "bet"],
-      },
-      [1, 0]
-    );
-    this.strategy.update(
-      {
-        hand: "K",
-        actions: ["pass", "bet"],
-      },
-      [1, 0]
-    );
-
-    // for (const entry of this.beliefs._infoSet.keys()) {
-    //   console.log(entry, this.beliefs._infoSet.get(entry));
-    // }
-    for (const entry of this.cumulativeWeights._infoSet.keys()) {
-      console.log(entry, this.cumulativeWeights._infoSet.get(entry));
-    }
-  }
-
-  initStrategy(current: GameNode | TerminalNode) {
-    if (current instanceof TerminalNode) return;
-
-    const cards: Card[] = ["J", "Q", "K"];
-
-    for (const c of cards) {
-      const rng = (Math.random() + Math.random()) / 2;
-      this.strategy.update({ hand: c, actions: current.history }, [
-        rng,
-        1 - rng,
-      ]);
-    }
-
-    this.initStrategy(current.pass);
-    this.initStrategy(current.bet);
-  }
-
-  getOpponentRange(node: GameNode, player: Card): number[] {
-    // If no action has been taken yet, than the initial expection is 50/50
-    if (node.history.length === 0) {
-      return [0.5, 0.5];
-    }
-
-    const cards = getOtherCards(player);
-    const history = [...node.history];
-    const lastAction = history.pop();
-
-    const range: number[] = [];
-    for (const c of cards) {
-      const [pass, bet] = this.strategy.get({
-        hand: c,
-        actions: history,
-      });
-
-      if (lastAction === "pass") range.push(pass);
-      else range.push(bet);
-    }
-    const sum = range.reduce((acc, r) => (acc += r));
-    return range.map((r) => r / sum);
-  }
-
-  updateBeliefs(current: GameNode | TerminalNode) {
-    if (current instanceof TerminalNode) return;
-
-    this.beliefs.update(
-      { hand: current.player, actions: current.history },
-      this.getOpponentRange(current, current.player)
-    );
-
-    this.updateBeliefs(current.pass);
-    this.updateBeliefs(current.bet);
-  }
-
-  getNodeEV(current: GameNode | TerminalNode, belief: number[]): number {
-    if (current instanceof TerminalNode) {
-      const oppRange = getOtherCards(current.player);
-      let ev = current.payoff(current.player, oppRange[0]) * belief[0];
-      ev += current.payoff(current.player, oppRange[1]) * belief[1];
-      return ev;
-    }
-
-    const passEV = this.getNodeEV(current.pass, belief);
-    const betEV = this.getNodeEV(current.bet, belief);
-    this.cumulativeWeights.update(
-      {
-        hand: current.player,
-        actions: current.history,
-      },
-      [passEV, betEV]
-    );
-    return passEV + betEV;
+  for (const key of game.infoSets.keys()) {
+    console.log(key, game.infoSets.get(key));
   }
 }
-
-type Key = { hand: Card; actions: Action[] };
 
 class InfoSet {
-  _infoSet: Map<string, number[]>;
-
+  strategy: number[];
+  regretSum: number[];
   constructor() {
-    this._infoSet = new Map();
-  }
-
-  get(key: Key) {
-    const v = this._infoSet.get(this._key(key));
-    if (v === undefined) {
-      throw new Error(`No key: '${this._key(key)}' for InfoSet`);
-    }
-    return v;
-  }
-
-  update(key: Key, value: number[]) {
-    this._infoSet.set(this._key(key), value);
-    return this.get(key);
-  }
-
-  private _key({ hand, actions }: Key): string {
-    return actions.reduce((acc: string, action: Action) => {
-      return action === "pass" ? acc + "p" : acc + "b";
-    }, hand);
+    this.strategy = [0.5, 0.5];
+    this.strategy = strategy;
+    this.regretSum = [0, 0];
   }
 }
--- src/game/node.ts
+++ src/game/node.ts
@@ -1,84 +1,29 @@
-import { CardRankings } from "./constants";
-import { Action, Card } from "./types";
+export class KhunNode {
+  regretSum: number[];
+  strategy: number[];
+  strategySum: number[];
+  numActions: number;
 
-interface KhunNode {
-  cards: [Card, Card];
-  player: Card;
-  history: Action[];
-}
-
-export class GameNode implements KhunNode {
-  // Each card represents a player
-  cards: [Card, Card];
-  player: Card;
-  pass: GameNode | TerminalNode;
-  bet: GameNode | TerminalNode;
-  history: Action[];
-
-  constructor(
-    cards: [Card, Card],
-    pass: GameNode | TerminalNode,
-    bet: GameNode | TerminalNode,
-    history: Action[]
-  ) {
-    this.cards = cards;
-
-    this.history = history;
-    this.player = this.history.length % 2 === 0 ? this.cards[0] : this.cards[1];
-
-    this.pass = pass;
-    this.bet = bet;
-  }
-}
-
-export class TerminalNode implements KhunNode {
-  cards: [Card, Card];
-  player: Card;
-
-  constructor(
-    cards: [Card, Card],
-    public history: Action[],
-    public type: "showdown" | "fold"
-  ) {
-    this.cards = cards;
-    this.history = history;
-
-    this.player = this.history.length % 2 === 0 ? this.cards[0] : this.cards[1];
+  constructor() {
+    this.regretSum = [0, 0];
+    this.strategy = [0, 0];
+    this.strategySum = [0, 0];
+    this.numActions = 2;
   }
 
-  payoff(player: Card, opponent: Card) {
-    const lastAction = this.history[this.history.length - 1];
-    let v = 1;
-    if (this.type === "fold") {
-      return player === this.player ? v : -v;
-    }
-    if (lastAction === "bet") {
-      v = 2;
-    }
-    return CardRankings[player] > CardRankings[opponent] ? v : -v;
-  }
-}
+  getStrategy() {
+    let normalizingSum = 0;
+    for (let i = 0; i < this.numActions; i++) {
+      if (this.regretSum[i] > 0) this.strategy[i] = this.regretSum[i];
+      else this.strategy[i] = 0;
 
-export function nodeFactory(cards: [Card, Card]): GameNode {
-  return createNodeTree([]);
-  function createNodeTree(history: Action[]): GameNode {
-    const last = history.length - 1;
-
-    if (history[last] === "pass") {
-      // Both players pass
-      const pass = new TerminalNode(cards, [...history, "pass"], "showdown");
-      const bet = createNodeTree([...history, "bet"]);
-      return new GameNode(cards, pass, bet, history);
+      normalizingSum += this.strategy[i];
     }
-    if (history[last] === "bet") {
-      const pass = new TerminalNode(cards, [...history, "pass"], "fold");
-      const bet = new TerminalNode(cards, [...history, "bet"], "showdown");
-
-      return new GameNode(cards, pass, bet, history);
+    for (let i = 0; i < this.numActions; i++) {
+      if (normalizingSum > 0) this.strategy[i] /= normalizingSum;
+      else this.strategy[i] = 1 / this.numActions;
     }
 
-    const pass = createNodeTree([...history, "pass"]);
-    const bet = createNodeTree([...history, "bet"]);
-    return new GameNode(cards, pass, bet, history);
+    return this.strategy;
   }
 }
